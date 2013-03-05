7387bdc1adecfd12894047810f7f3e4e92598c7d
Chris Bachmann <chris@ubunturails.(none)>
2013-03-05 13:16:02 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '083cc426338b7a632c904ee2bbb1d7d6295f02da34f4f1a4317af6a0298023cb5124c7a22712881d594f93e469f4ac61cbcf8cc641cc4894c8571ba069402d5a'
