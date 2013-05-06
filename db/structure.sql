d3066ed22c8d73c194667fffb0897df6c162c225
Chris Bachmann <chris@ubunturails.(none)>
2013-05-06 14:55:52 +0200

    Lektion 10

---
+++ db/structure.sql
@@ -0,0 +1,9 @@
+INSERT INTO schema_migrations (version) VALUES ('20130312122911');
+
+INSERT INTO schema_migrations (version) VALUES ('20130430110440');
+
+INSERT INTO schema_migrations (version) VALUES ('20130430111411');
+
+INSERT INTO schema_migrations (version) VALUES ('20130430113535');
+
+INSERT INTO schema_migrations (version) VALUES ('20130430114614');
